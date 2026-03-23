f74f78ee62c1798967e541a94517421654a79749
ngygbr01 <ngygbr01@gmail.com>
2026-03-23 20:06:21 +0100

    v5

---
+++ words.js
@@ -0,0 +1,17 @@
+const wordPairs = [
+    { a: "Szegedi halászlé", b: "Bajai halászlé" },
+    { a: "Szegedi Dóm", b: "Esztergomi Bazilika" },
+    { a: "Kárász utca", b: "Váci utca" },
+    { a: "Szegedi papucs", b: "Gumicsizma" },
+    { a: "Pick Szalámi", b: "Herz Szalámi" },
+    
+    { a: "Faszkalap", b: "Seggfej" },
+    { a: "Kurva", b: "Rongy" },
+    { a: "Beszari", b: "Fosós" },
+    
+    { a: "Zokniszandál", b: "Crocs" },
+    { a: "Mamahotel", b: "Kollégium" },
+    { a: "Sörhas", b: "Kockahas" },
+    { a: "Puki", b: "Fing" },
+    { a: "Másnaposság", b: "Részegség" }
+];
